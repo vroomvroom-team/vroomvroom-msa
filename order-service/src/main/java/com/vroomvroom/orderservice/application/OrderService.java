d94ed6bad8857a15f41e1dc45b84d08d3817458d
Wongyu Lee <bitamin707@naver.com>
2025-11-07 15:10:02 +0900

    [FEAT] 주문 생성, 조회 API 구현 (#13)
    * [feat] 주문 추가
    
    * [fix] Order 엔티티 PK id명 수정
    
    * [feat] 주문 생성, 조회 기능 구현
    
    * [fix] DTO Request, Response 명칭 수정, Service, ServiceImpl 구조 수정

--- order-service/src/main/java/com/vroomvroom/orderservice/application/OrderService.java
+++ order-service/src/main/java/com/vroomvroom/orderservice/application/OrderService.java
@@ -1,4 +1,17 @@
 package com.vroomvroom.orderservice.application;
 
+
+import com.vroomvroom.orderservice.application.command.CreateOrderCommand;
+import com.vroomvroom.orderservice.application.dto.OrderRes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
 public interface OrderService {
+    OrderRes createOrder(CreateOrderCommand command);
+
+    OrderRes getOrder(UUID orderId);
+
+    Page<OrderRes> getOrders(Pageable pageable);
 }
