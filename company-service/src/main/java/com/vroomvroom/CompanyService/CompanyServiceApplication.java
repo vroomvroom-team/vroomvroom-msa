b0dd0af1b9c20c82c83cb6419b80f2897479c3c4
alscksdlek <mseon916@naver.com>
2025-11-08 00:38:05 +0900

    [feat] 업체 프로젝트 세팅 및 환경 구성

---
+++ company-service/src/main/java/com/vroomvroom/CompanyService/CompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vroomvroom.CompanyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+
+}
