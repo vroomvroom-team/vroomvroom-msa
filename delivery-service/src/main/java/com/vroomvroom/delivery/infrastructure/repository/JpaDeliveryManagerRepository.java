2c469b1e382a025c2470a3a3b2bd3459a7f5dbf7
minahYu <mina010311@naver.com>
2025-11-08 03:24:59 +0900

    [feat] 배송담당자 Repository 추가

---
+++ delivery-service/src/main/java/com/vroomvroom/delivery/infrastructure/repository/JpaDeliveryManagerRepository.java
@@ -0,0 +1,42 @@
+package com.vroomvroom.delivery.infrastructure.repository;
+
+import com.vroomvroom.delivery.domain.entity.DeliveryManager;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface JpaDeliveryManagerRepository extends JpaRepository<DeliveryManager, Long> {
+
+    boolean existsById(Long userId);
+
+
+    // 허브 매니저(전역)
+    @Query(value = """
+               SELECT s.seq
+               FROM generate_series(0, 9) AS s(seq)
+               LEFT JOIN p_delivery_manager dm
+                      ON dm.sequence = s.seq
+                     AND dm.type = 'HUB_MANAGER'
+                     AND dm.deleted_at IS NULL
+               WHERE dm.sequence IS NULL
+               ORDER BY s.seq
+               LIMIT 1
+        """, nativeQuery = true)
+    Long nextGlobalSequence();
+
+    // 업체 매니저(허브별)
+    @Query(value = """
+               SELECT s.seq
+               FROM generate_series(0, 9) AS s(seq)
+               LEFT JOIN p_delivery_manager dm
+                      ON dm.sequence = s.seq
+                     AND dm.type = 'COMPANY_MANAGER'
+                     AND dm.hub_id = :hubId
+                     AND dm.deleted_at IS NULL
+               WHERE dm.sequence IS NULL
+               ORDER BY s.seq
+               LIMIT 1
+        """, nativeQuery = true)
+    Long nextHubSequence(@Param("hubId") UUID hubId);
+}
