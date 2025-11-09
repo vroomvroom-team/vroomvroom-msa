abedc9258e42c2388555d2d6cd30137bb72a942b
kinhyunsu <kimhyunsoo0720@gmail.com>
2025-11-09 15:41:28 +0900

    초기설정

---
+++ slack-service/src/main/java/com/vroomvroom/slackservice/SlackServiceApplication.java
@@ -0,0 +1,11 @@
+package com.vroomvroom.slackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SlackServiceApplication.class, args);
+	}
+}
---
+++ slack-service/src/test/java/com/vroomvroom/slackservice/SlackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vroomvroom.slackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
