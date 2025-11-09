20183248e2a0d332d61d2eb39754176efdd798b0
kinhyunsu <kimhyunsoo0720@gmail.com>
2025-11-09 15:41:28 +0900

    초기설정

--- eureka-server/src/main/java/com/vroomvroom/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/vroomvroom/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package java.com.vroomvroom.eureka;
+package com.vroomvroom.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
