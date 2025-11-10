0d312e4a703d6a6df874ab9108476aa5d20a44ae
Sojinyoung <wo0982@naver.com>
2025-11-10 09:39:35 +0900

    [feat] User 엔티티 추가

---
+++ user-service/src/main/java/com/vroomvroom/user/domain/model/UserStatus.java
@@ -0,0 +1,7 @@
+package com.vroomvroom.user.domain.model;
+
+public enum UserStatus {
+	PENDING,     // 승인 대기
+	APPROVED,    // 승인 완료
+	REJECTED     // 가입 거절
+}
---
+++ user-service/src/main/java/com/vroomvroom/user/domain/vo/SlackId.java
@@ -0,0 +1,40 @@
+package com.vroomvroom.user.domain.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class SlackId {
+
+	@Column(name = "slack_id", nullable = false)
+	private String value;
+
+	public SlackId(String value) {
+		if (value == null || value.isBlank()) {
+			throw new IllegalArgumentException("Slack ID는 필수값입니다.");
+		}
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof SlackId slackId)) return false;
+		return value.equals(slackId.value);
+	}
+
+	@Override
+	public int hashCode() {
+		return value.hashCode();
+	}
+}
