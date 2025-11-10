e73c6b14a704ae08f3a5b51e95f50390cf49bca9
alscksdlek <mseon916@naver.com>
2025-11-10 23:47:48 +0900

    [fix] 충돌 해결

--- company-service/src/main/java/com/vroomvroom/company/domain/entity/Company.java
+++ company-service/src/main/java/com/vroomvroom/company/domain/entity/Company.java
@@ -100,24 +100,4 @@ public class Company extends BaseTimeEntity {
     public void changeType(CompanyType companyType) {
         this.companyType = companyType;
     }
-
-    public void changeHub(UUID hubId) {
-        this.hubId = hubId;
-    }
-
-    public void changeManager(Long companyManagerId) {
-        this.companyManagerId = companyManagerId;
-    }
-
-    public void changeName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public void changeAddress(String companyAddress) {
-        this.companyAddress = companyAddress;
-    }
-
-    public void changeType(CompanyType companyType) {
-        this.companyType = companyType;
-    }
 }
