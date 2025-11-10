3e442258ba3155e077183e8fb0f1dc4686c7bcdc
minahYu <mina010311@naver.com>
2025-11-11 03:06:49 +0900

    [feat] 배송 생성 관련 ErrorCode 추가 및 수정

--- delivery-service/src/main/java/com/vroomvroom/delivery/domain/exception/DeliveryErrorCode.java
+++ delivery-service/src/main/java/com/vroomvroom/delivery/domain/exception/DeliveryErrorCode.java
@@ -13,9 +13,16 @@ public enum DeliveryErrorCode {
     INVALID_MANAGER_TYPE(HttpStatus.BAD_REQUEST, "잘못된 담당자 타입입니다."),
     MANAGER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 배송매니저입니다."),
     HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 허브 ID입니다."),
-    DELIVERY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 배송 담당자 ID입니다."),
+    DELIVERY_MANAGER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 배송 담당자 ID입니다."),
     HUB_MANAGER_CAPACITY_FULL(HttpStatus.CONFLICT, "허브 매니저 정원이 초과되었습니다."),
     COMPANY_MANAGER_CAPACITY_FULL(HttpStatus.CONFLICT, "해당 허브의 업체매니저 정원이 초과되었습니다."),
+
+    // Delivery
+    START_HUB_EQUALS_ARRIVE_HUB(HttpStatus.BAD_REQUEST, "출발 허브와 도착 허브는 같을 수 없습니다."),
+    ORDER_STATE_CONFLICT(HttpStatus.CONFLICT, "배송 불가능한 주문입니다."),
+    ORDER_NOT_FOUND(HttpStatus.CONFLICT, "존재하지 않는 주문입니다."),
+    DELIVERY_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 배송경로입니다."),
+
     ;
 
     private final HttpStatus httpStatus;
